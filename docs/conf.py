28b31e469484fa255eec5b1067c39ebf166dcad7
vherremo <vherremo@emeal.nttdata.com>
2026-05-04 18:12:06 +0200

    Redo of navigation

--- docs/conf.py
+++ docs/conf.py
@@ -28,3 +28,19 @@ root_doc = 'index1'
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+html_theme_options = {
+    'analytics_id': '',  # Add your GA4 ID if needed
+    'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'prev_next_buttons_location': 'bottom',  # Show prev/next navigation
+    'style_external_links': True,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': '#2980B9',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False,
+}
