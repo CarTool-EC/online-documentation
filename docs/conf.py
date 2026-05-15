f1ba71faf60e3d2229e7c46005c348d9622f350b
vherremo <vherremo@emeal.nttdata.com>
2026-05-15 14:48:39 +0200

    CSS update
    Improvement of CSS

--- docs/conf.py
+++ docs/conf.py
@@ -6,8 +6,8 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'EIRA & eGovERA Documentation'
-copyright = '© Copyright 2026, European Commission, DIGIT.'
+project = 'Reference Architecture Specification: EIRA & eGovERA'
+copyright = '© Copyright 2026, European Commission, DIGIT'
 html_show_sphinx = False
 author = 'Interoperability Architecture Solutions Action'
 release = '1.0.0'
@@ -28,12 +28,13 @@ root_doc = 'index1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_logo = 'images/media/logo.png'
 html_static_path = ['_static']
 html_css_files = ['custom.css']
 
 html_theme_options = {
     'analytics_anonymize_ip': False,
-    'logo_only': True,
+    'logo_only': False,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': True,
     'vcs_pageview_mode': '',
